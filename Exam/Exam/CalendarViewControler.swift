cbba2b0bb043196382d019eed235d61a2e177324
IvoAtovsky <ivoatovsky2@gmail.com>
2022-01-07 19:56:27 +0200

    collection view stuff

--- Exam/Exam/CalendarViewControler.swift
+++ Exam/Exam/CalendarViewControler.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class CalendarViewControler: UIViewController {
 
+	@IBOutlet weak var calendarView: UICollectionView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
@@ -16,4 +17,22 @@ class CalendarViewControler: UIViewController {
 	}
 
 
+}
+
+extension CalendarViewControler : UICollectionViewDataSource{
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 31 // dnite
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		var myCell = CVewCell()
+		
+		if let cell = calendarView.dequeueReusableCell(withReuseIdentifier: "day", for: indexPath) as? CVewCell{
+			myCell = cell
+		}
+		
+		return myCell
+	}
+	
+	
 }
---
+++ Exam/Exam/ColectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ColectionViewCell.swift
+//  Exam
+//
+//  Created by Nina Ivanova on 7.01.22.
+//
+
+import UIKit
+
+class CVewCell: UICollectionViewCell {
+	
+	@IBOutlet weak var day: UILabel!
+	@IBOutlet weak var time: UILabel!
+	@IBOutlet weak var title: UILabel!
+}
